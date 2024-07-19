dba99d76b104b4d4e9ee180792cc1b658f26b42a
saptarsidebnath98 <saptarsidebnath666666@gmail.com>
2024-07-19 14:47:05 +0530

    random function is created

--- src/components/GamePlay.jsx
+++ src/components/GamePlay.jsx
@@ -1,15 +1,27 @@
+import { useState } from "react";
 import Button from "./Button";
 
 const GamePlay = () => {
+    const [randVal, setRandVal] = useState(1);
 
+    function getRandomVal(min, max){
+        return Math.floor(Math.random() * (max - min) + min); 
+    }
     const btns = [1, 2, 3, 4, 5, 6];
 
+
+
+    const onDiceClick = () => {
+        setRandVal(getRandomVal(1, 7))
+        console.log(randVal);
+    }
+
     const clickBtn = (e) => {
-        console.log(e.target.value)
+        // console.log(e.target.value)
     }
     return (
-        <div className="flex flex-col bg-gray-700 w-[80%] gap-5">
-            <div className="flex justify-between py-2 h-[151px] bg-red-300">
+        <div className="flex flex-col w-[80%] gap-5">
+            <div className="flex justify-between py-2 h-[151px]">
                 <div className="flex flex-col items-center justify-between">
                     <h1 className="text-8xl my-0">0</h1>
                     <p className="font-semibold">Total Score</p>
@@ -25,8 +37,10 @@ const GamePlay = () => {
             </div>
 
             <div className="flex flex-col items-center gap-[36px]">
-                <img src="images/dice_1.png" alt="dice1" className="h-[250px] w-[250px]"/>
-                <p>click on Dice to roll</p>
+                <button onClick={onDiceClick}>
+                    <img src="images/dice_1.png" alt="dice1" className="h-[250px] w-[250px]"/>
+                </button>
+                <p className="font-semibold text-xl">click on Dice to roll</p>
                 <Button text="Reset Score"/>
                 <Button text="Show Rules" outLine={true}/>
             </div>
