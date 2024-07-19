81db4ca18a861210feafadfa307ac6f2367a66c8
saptarsidebnath98 <saptarsidebnath666666@gmail.com>
2024-07-19 13:43:43 +0530

    starting Page created

---
+++ src/components/StartingPage.jsx
@@ -0,0 +1,16 @@
+import Button from "./Button";
+const StartingPage = ({onClick}) => {
+  return (
+    <div className="w-[1182px] flex justify-between items-center bg-green-400">
+        <div>
+        <img src="images/dices 1.png" alt="" />
+        </div>
+        <div className="flex flex-col items-end">
+            <h1 className="text-[96px] font-bold">DICE GAME</h1>
+            <Button text="Play Now" onClick={onClick}/>
+        </div>
+    </div>
+  )
+}
+
+export default StartingPage
