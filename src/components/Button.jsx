534614197df32c3195687a01781595c41765bbed
saptarsidebnath98 <saptarsidebnath666666@gmail.com>
2024-07-19 13:42:45 +0530

    project set up

---
+++ src/components/Button.jsx
@@ -0,0 +1,11 @@
+
+const Button = ({text, onClick, outLine}) => {
+  return (
+    <button 
+    className={`w-[220px] h-[44px] flex justify-center items-center rounded-md text-sm font-Poppins px-3 py-5 border-2 border-black font-bold
+    ${outLine ? 'bg-white text-black hover:bg-black hover:text-white' : 'bg-black text-white hover:bg-white hover:text-black'}`}
+    onClick={onClick}>{text}</button>
+  )
+}
+
+export default Button
